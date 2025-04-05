261e003f4363ccb0b4625059d153cd3680887def
rowdypiperprogramming <s2005493@online.houstonisd.org>
2025-04-05 08:41:33 -0500

    Create WorkerDB.sql

---
+++ WorkerDB.sql
@@ -0,0 +1,5 @@
+create table workers(  id integer, name varhcar(30),username varchar(30),langs varchar(100),);
+insert into wokers(id, name, username, langs)
+values(1, "Rowdy Piper", "rowdypiperprogramming","py, sql, html, css, bash");
+insert into wokers(id, name, username, langs)
+values(2, "Ryan Liu", "rpdsworker1","py");
