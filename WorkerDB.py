87377f604baf0ba532ccecd5bc979670d5944e17
rowdypiperprogramming <s2005493@online.houstonisd.org>
2025-04-07 09:06:57 -0500

    Create WorkerDB.py

---
+++ WorkerDB.py
@@ -0,0 +1,14 @@
+workers = [
+    {
+        "id": 1,
+        "name": "Rowdy Piper",
+        "username": "rowdypiperprogramming",
+        "langs": ["py", "sql", "html", "css", "bash"]
+    },
+    {
+        "id": 2,
+        "name": "Ryan Liu",
+        "username": "rpdsworker1",
+        "langs": ["py"]
+    }
+]
