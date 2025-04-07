2bf6caec5da8642cfd7f774969d1a2ff1664d9fe
rowdypiperprogramming <s2005493@online.houstonisd.org>
2025-04-07 09:11:46 -0500

    Update WorkerDB.sql

--- WorkerDB.sql
+++ WorkerDB.sql
@@ -1,9 +1,12 @@
--- your code goes here
-create table workers(  id integer, name varcharar(30),username varchar(30),langs varchar(100));
-insert into workers(id, name, username, langs)
-values(1, "Rowdy Piper", "rowdypiperprogramming","py, sql, html, css, bash");
-insert into workers(id, name, username, langs)
-values(2, "Ryan Liu", "rpdsworker1","py");
+CREATE TABLE workers (
+    id INT,
+    name VARCHAR(30),
+    username VARCHAR(30),
+    langs VARCHAR(100)
+);
 
-select*
-from workers
+INSERT INTO workers (id, name, username, langs)
+VALUES (1, 'Rowdy Piper', 'rowdypiperprogramming', 'py, sql, html, css, bash');
+
+INSERT INTO workers (id, name, username, langs)
+VALUES (2, 'Ryan Liu', 'rpdsworker1', 'py');
